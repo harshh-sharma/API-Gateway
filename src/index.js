758ebcf11c4fbf74e00554e19f45576a4f6a2344
Harsh Sharma <124427013+harshh-sharma@users.noreply.github.com>
2025-01-07 01:34:52 +0530

    adding Api rate limiting in Api gateway

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const { PORT } = require("./config");
 const apiRouter = require("./routes");
+const { default: rateLimit } = require("express-rate-limit");
 
 const app = express();
 
+const limiter = rateLimit({
+    windowMs:2*60*1000 ,// 2 minute
+    max:4
+})
+
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+
+app.use(limiter);
 
 app.use('/api',apiRouter);
 
